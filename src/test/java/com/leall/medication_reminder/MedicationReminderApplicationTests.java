b04abde5ec6f4481410107d94f288fc0e16036d4
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-10-26 00:21:44 -0300

    first commit

---
+++ src/test/java/com/leall/medication_reminder/MedicationReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leall.medication_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
